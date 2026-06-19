718ca77657ee77e6f6f02221e66d08874055088d
Xaviervera214 <158114417+Xaviervera214@users.noreply.github.com>
2026-06-19 02:07:50 +0000

    Top productos criticos y top valor por pedir

--- inventario.py
+++ inventario.py
@@ -36,8 +36,28 @@ def valor_total_por_pedir():
     total = productos["valor_por_pedir"].sum()
     return total
 
+def top_productos_criticos():
+    
+    productos = productos_por_pedir()
+    top = productos.sort_values(by="por_pedir", ascending=False)
+    return top.head(10)
+
+def top_valor_por_pedir():
+
+    productos = productos_por_pedir()
+    top = productos.sort_values(by="valor_por_pedir", ascending=False)
+    return top.head(10)
+
 productos = productos_por_pedir()
 total = valor_total_por_pedir()
 
 print("Productos por pedir:", len(productos))
 print(f"Valor total por pedir: ${total:,.2f}")
+
+print("\nTop productos críticos:")
+print(top_productos_criticos())
+
+
+
+print("\nTop valor por pedir:")
+print(top_valor_por_pedir())
